35264b152fe17a0d53a0fb3248f15f9089dcb432
Fabiow Quixadá <ftquixada@gmail.com>
2018-03-29 21:31:04 -0300

    "<br/>" Tag Rule to Multiline Checking

--- src/spec/app/rules/BrTagRuleSpec.js
+++ src/spec/app/rules/BrTagRuleSpec.js
@@ -2,6 +2,7 @@ const reqlib = require('app-root-path').require;
 const SpecHelper = reqlib('/src/spec/SpecHelper');
 const specFileName = require('path').basename(__filename);
 const rule = reqlib('/src/spec/SpecHelper').getRule(specFileName);
+const FileParser = reqlib('/src/app/FileParser');
 
 describe(rule.name, () => {
     beforeEach(() => {
@@ -13,14 +14,14 @@ describe(rule.name, () => {
     });
 
     it('detects inadequate code', () => {
-        let line = '<br';
+        const file = SpecHelper.getRuleSpecTemplate(rule, 0);
 
-        expect(rule.isBroken(line)).toBe(true);
+        expect(rule.check(file, FileParser)).toBe(true);
     });
 
     it('accepts code that is not related to the rule', () => {
-        const line = 'if (category == null && pdict.Product != null) {';
+        const file = SpecHelper.getRuleSpecTemplate(rule, 1);
 
-        expect(rule.isBroken(line)).toBe(false);
+        expect(rule.check(file, FileParser)).toBe(false);
     });
 });
