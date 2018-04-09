e86f23517e7a300419ae8809f94543cad4228028
Fabiow Quixadá <ftquixada@gmail.com>
2018-04-09 17:22:21 -0300

    Config Loader Refactoring

--- src/app/ConfigLoader.js
+++ src/app/ConfigLoader.js
@@ -2,8 +2,9 @@ const path = require('path');
 const Constants = require('./Constants');
 
 const loadCurrentEnvConfigurationFile = () => {
+
     if (process.env.NODE_ENV === Constants.ENV_TEST) {
-        return require('../spec/spec_config.json');
+        return require(path.join(Constants.specDir, Constants.specConfigFileName));
     }
 
     return require(path.join(Constants.clientAppDir, Constants.clientConfigFileName));
