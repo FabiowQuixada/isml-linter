cecd7f95a93a58a9689b1f0897ddaaacd2658303
Fabiow Quixadá <ftquixada@gmail.com>
2018-08-16 23:38:38 -0300

    Occurrence Position and Length - Dw Occurrence Rule

--- src/app/rules/DwOccurrenceRule.js
+++ src/app/rules/DwOccurrenceRule.js
@@ -2,11 +2,30 @@ const SingleLineRulePrototype = require('./prototypes/SingleLineRulePrototype');
 
 const ruleName = require('path').basename(__filename).slice(0, -3);
 const description = 'Avoid direct call to the "dw" package, use "require()" instead';
+const regex = /dw\.[A-Za-z]+\.[A-Za-z]+(\.|;)/;
 
 const Rule = Object.create(SingleLineRulePrototype);
 
 Rule.init(ruleName, description);
 
-Rule.isBroken = function(line) { return line.indexOf('dw.') !== -1; };
+Rule.isBroken = function(line) { return line.match(regex); };
+
+Rule.getFirstOccurrence = function(line) {
+
+    let result = null;
+
+    if (this.isBroken(line)) {
+
+        const temp = regex.exec(line);
+        const matchPos = temp.index;
+
+        result = {
+            columnStart: matchPos,
+            length: temp[0].length -  1
+        };
+    }
+
+    return result;
+};
 
 module.exports = Rule;
--- src/spec/app/rules/DwOccurrenceRuleSpec.js
+++ src/spec/app/rules/DwOccurrenceRuleSpec.js
@@ -21,8 +21,14 @@ describe(rule.name, () => {
     it('detects inadequate code in the middle of the line', () => {
         const fileContent = SpecHelper.getRuleSpecTemplateContent(rule, 1);
         const result = rule.check(fileContent);
+        const expectedResult = [{
+            line: '<a href="${dw.catalog.ProductSearchModel.urlForCategory(\'Search-Show\',cat.ID)}"',
+            lineNumber: 0,
+            columnStart: 11,
+            length: 29
+        }];
 
-        expect(result.occurrences).not.toEqual([]);
+        expect(result.occurrences).not.toEqual(expectedResult);
     });
 
     it('accepts good code', () => {
@@ -37,5 +43,18 @@ describe(rule.name, () => {
         const result = rule.check(fileContent);
 
         expect(result.occurrences).toEqual([]);
+    });
+
+    it('detects inadequate code upon declaration and no assignment', () => {
+        const fileContent = SpecHelper.getRuleSpecTemplateContent(rule, 4);
+        const result = rule.check(fileContent);
+        const expectedResult = [{
+            line: 'const productLineItem : dw.order.ProductLineItem; // Some comment',
+            lineNumber: 1,
+            columnStart: 25,
+            length: 24
+        }];
+
+        expect(result.occurrences).toEqual(expectedResult);
     });
 });
