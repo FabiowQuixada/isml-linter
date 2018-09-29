22b47cb22096af7bcfe3b29c9fc5863187142955
Fabiow Quixadá <ftquixada@gmail.com>
2018-09-29 00:47:01 -0300

    Automate ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,26 @@ const Constants = require('./src/app/Constants');
 const config = require('./.ismllinter.json');
 
 const ismlRegex = config.rootTemplateDir + '**/*.isml';
- 
+
 gulp.task('lint_n_fix', () => {
-    return gulp.src([Constants.srcJsRegex,'!node_modules/**', `!${Constants.specTempDir}`])
+    return gulp.src([Constants.srcJsRegex, '!node_modules/**', `!${Constants.specTempDir}`])
         .pipe(eslint({ fix: true }))
         .pipe(eslint.format())
-        .pipe(eslintIfFixed( file => (file.base)));
+        .pipe(eslintIfFixed( file => file.base));
 });
- 
+
 gulp.task('test', () =>
     gulp.src(Constants.specJsRegex)
         .pipe(jasmine())
 );
 
-gulp.task('pre-cover', () => 
+gulp.task('pre-cover', () =>
     gulp.src(Constants.srcJsRegex)
         .pipe(istanbul())
         .pipe(istanbul.hookRequire())
 );
 
-gulp.task('cover', ['pre-cover'], () => 
+gulp.task('cover', ['pre-cover'], () =>
     gulp.src(Constants.srcJsRegex)
         .pipe(jasmine())
         .pipe(istanbul.writeReports())
@@ -38,21 +38,21 @@ gulp.task('lint_isml', () => run('npm start').exec() );
 
 /** Watchers *************************************************************************************/
 gulp.task('watch_n_fix', () => {
-  gulp.watch(Constants.srcJsRegex, ['lint_n_fix']);
+    gulp.watch(Constants.srcJsRegex, ['lint_n_fix']);
 });
 
 gulp.task('watch_n_cover', () => {
-  gulp.watch(Constants.srcJsRegex, ['cover']);
+    gulp.watch(Constants.srcJsRegex, ['cover']);
 });
 
 gulp.task('watch_n_test', () => {
-  gulp.watch(Constants.srcJsRegex, ['test']);
+    gulp.watch(Constants.srcJsRegex, ['test']);
 });
 
 gulp.task('watch_fix_n_test', () => {
-  gulp.watch(Constants.srcJsRegex, ['lint_n_fix', 'test']);
+    gulp.watch(Constants.srcJsRegex, ['lint_n_fix', 'test']);
 });
 
 gulp.task('watch_isml', () => {
-  gulp.watch(ismlRegex, ['lint_isml']);
+    gulp.watch(ismlRegex, ['lint_isml']);
 });
