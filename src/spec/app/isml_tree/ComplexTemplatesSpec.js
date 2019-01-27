63d34156453705d392734245fa4ef88520fc6e98
Fabiow Quixadá <ftquixada@gmail.com>
2019-01-26 21:55:06 -0300

    Isml Tree Build - Add Complex Templates Specs

--- src/spec/app/isml_tree/ComplexTemplatesSpec.js
+++ src/spec/app/isml_tree/ComplexTemplatesSpec.js
@@ -1,6 +1,7 @@
 const TreeBuilder = require('../../../app/isml_tree/TreeBuilder');
 const SpecHelper  = require('../../SpecHelper');
 const Constants   = require('../../../app/Constants');
+const ParseStatus = require('../../../app/enums/ParseStatus');
 
 const targetObjName = SpecHelper.getTargetObjName(__filename);
 
@@ -15,7 +16,8 @@ describe(targetObjName, () => {
     });
 
     it('parses a complex template I', () => {
-        const rootNode     = TreeBuilder.build(getFilePath(0)).rootNode;
+        const result       = TreeBuilder.build(getFilePath(0));
+        const rootNode     = result.rootNode;
         const commentNode  = rootNode.getChild(2);
         const setNode      = rootNode.getChild(7);
         const trNode       = rootNode.getChild(9);
@@ -25,6 +27,7 @@ describe(targetObjName, () => {
         const nestedIfNode = ifNode.getChild(1).getChild(0);
         const lastTdNode   = rootNode.getChild(11);
 
+        expect(result.status).toEqual(ParseStatus.NO_ERRORS);
         expect(rootNode.getNumberOfChildren()).toEqual(12);
 
         expect(commentNode.getValue()).toEqual('\n\n<iscomment>');
@@ -62,6 +65,102 @@ describe(targetObjName, () => {
         expect(lastTdNode.getValue()).toEqual('\n<td class="item_total">');
         expect(lastTdNode.getLineNumber()).toEqual(102);
         expect(lastTdNode.getNumberOfChildren()).toEqual(1);
+    });
+
+    it('parses a complex template II', () => {
+        const result          = TreeBuilder.build(getFilePath(1));
+        const rootNode        = result.rootNode;
+        const firstDivNode    = rootNode.getChild(1);
+        const htmlCommentNode = rootNode.getChild(4);
+        const errorMsgNode    = rootNode.getChild(5).getChild(1).getChild(0).getChild(2);
+        const inputNode       = rootNode.getChild(6).getChild(1).getChild(0).getChild(1);
+        const inputValue      = '\n' +
+        '            <input type="text" class="form-control billingZipCode" id="billingZipCode"\n' +
+        '                value="${pdict.order.billing.billingAddress.address\n' +
+        '                    && pdict.order.billing.billingAddress.address.postalCode\n' +
+        '                    ? pdict.order.billing.billingAddress.address.postalCode\n' +
+        '                    : \'\'}"\n' +
+        '                <isprint value=${billingFields.postalCode.attributes} encoding="off"/>\n' +
+        '                autocomplete="billing postal-code"/>';
+
+        expect(result.status).toEqual(ParseStatus.NO_ERRORS);
+        expect(rootNode.getNumberOfChildren()).toEqual(7);
+
+        expect(firstDivNode.getValue()).toEqual('\n<div class="row">');
+        expect(firstDivNode.getLineNumber()).toEqual(2);
+        expect(firstDivNode.getNumberOfChildren()).toEqual(2);
+
+        expect(htmlCommentNode.getValue()).toEqual('\n\n<!--- make drop down -->');
+        expect(htmlCommentNode.getLineNumber()).toEqual(71);
+        expect(htmlCommentNode.getNumberOfChildren()).toEqual(0);
+
+        expect(errorMsgNode.getValue()).toEqual('\n            <div class="invalid-feedback">');
+        expect(errorMsgNode.getLineNumber()).toEqual(116);
+        expect(errorMsgNode.getNumberOfChildren()).toEqual(0);
+
+        expect(inputNode.getValue()).toEqual(inputValue);
+        expect(inputNode.getLineNumber()).toEqual(142);
+        expect(inputNode.getNumberOfChildren()).toEqual(0);
+    });
+
+    it('parses a complex template III', () => {
+        const result          = TreeBuilder.build(getFilePath(2));
+        const rootNode        = result.rootNode;
+        const loopNode        = rootNode.getChild(0);
+        const sectionNode     = loopNode.getChild(0).getChild(0).getChild(0).getChild(4).getChild(0).getChild(1);
+        const commentNode     = sectionNode.getChild(0).getChild(0).getChild(0).getChild(0).getChild(0).getChild(0).getChild(1).getChild(0).getChild(1);
+        const modalFooterNode = rootNode.getChild(1);
+        const otherLoopNode   = modalFooterNode.getChild(0).getChild(0).getChild(1).getChild(1);
+        const iNode           = otherLoopNode.getChild(0).getChild(1).getChild(0);
+
+        expect(loopNode.getValue()).toEqual('<isloop items="${pdict.products}" var="product" status="productLoopStatus">');
+        expect(loopNode.getLineNumber()).toEqual(1);
+        expect(loopNode.getNumberOfChildren()).toEqual(1);
+
+        expect(sectionNode.getValue()).toEqual('\n                        <section class="attributes">');
+        expect(sectionNode.getLineNumber()).toEqual(14);
+        expect(sectionNode.getNumberOfChildren()).toEqual(1);
+
+        expect(commentNode.getValue()).toEqual('\n                                                        <!-- Quantity Drop Down Menu -->');
+        expect(commentNode.getLineNumber()).toEqual(26);
+        expect(commentNode.getNumberOfChildren()).toEqual(0);
+
+        expect(modalFooterNode.getValue()).toEqual('\n\n<div class="modal-footer">');
+        expect(modalFooterNode.getLineNumber()).toEqual(94);
+        expect(modalFooterNode.getNumberOfChildren()).toEqual(1);
+
+        expect(otherLoopNode.getValue()).toEqual('\n                <isloop items="${pdict.selectedBonusProducts}" var="selectedProduct" status="productLoopStatus">');
+        expect(otherLoopNode.getLineNumber()).toEqual(102);
+        expect(otherLoopNode.getNumberOfChildren()).toEqual(1);
+
+        expect(iNode.getValue()).toEqual('<i class="fa fa-times" aria-hidden="true">');
+        expect(iNode.getLineNumber()).toEqual(109);
+        expect(iNode.getNumberOfChildren()).toEqual(0);
+    });
+
+    it('parses a complex template IV', () => {
+        const result             = TreeBuilder.build(getFilePath(3));
+        const rootNode           = result.rootNode;
+        const setNode            = rootNode.getChild(0);
+        const cardNumberRowNode  = rootNode.getChild(3);
+        const phoneRowNode       = rootNode.getChild(6);
+        const tooltipMessageNode = phoneRowNode.getChild(1).getChild(0).getChild(1).getChild(1).getChild(0);
+
+        expect(setNode.getValue()).toEqual('<isset name="creditFields" value="${pdict.forms.billingForm.creditCardFields}" scope="page"/>');
+        expect(setNode.getLineNumber()).toEqual(1);
+        expect(setNode.getNumberOfChildren()).toEqual(0);
+
+        expect(cardNumberRowNode.getValue()).toEqual('\n\n<div class="row">');
+        expect(cardNumberRowNode.getLineNumber()).toEqual(8);
+        expect(cardNumberRowNode.getNumberOfChildren()).toEqual(1);
+
+        expect(phoneRowNode.getValue()).toEqual('\n\n<div class="row">');
+        expect(phoneRowNode.getLineNumber()).toEqual(94);
+        expect(phoneRowNode.getNumberOfChildren()).toEqual(2);
+
+        expect(tooltipMessageNode.getValue()).toEqual('\n                    ${Resource.msg(\'tooltip.phone.number\',\'creditCard\',null)}\n                ');
+        expect(tooltipMessageNode.getLineNumber()).toEqual(119);
+        expect(tooltipMessageNode.getNumberOfChildren()).toEqual(0);
     });
 });
 
