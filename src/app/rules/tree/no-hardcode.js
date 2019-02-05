1b1c9741a99d3f32ebd3a093a83a49ce1444c948
Fabiow Quixadá <ftquixada@gmail.com>
2019-02-04 22:06:35 -0300

    Introducing New Rule - No Hard-Code

---
+++ src/app/rules/tree/no-hardcode.js
@@ -0,0 +1,17 @@
+const TreeRulePrototype = require('../prototypes/TreeRulePrototype');
+
+const ruleName    = require('path').basename(__filename).slice(0, -3);
+const description = 'Hardcoded string is not allowed';
+
+const Rule = Object.create(TreeRulePrototype);
+
+Rule.init(ruleName, description);
+
+Rule.isBroken = function(node) {
+    return !node.isRoot() &&
+        !node.isTag() &&
+        !node.isExpression() &&
+        !node.isCommentContent();
+};
+
+module.exports = Rule;
---
+++ src/spec/app/rules/no-hardcode-Spec.js
@@ -0,0 +1,29 @@
+const specFileName = require('path').basename(__filename);
+const SpecHelper   = require('../../SpecHelper');
+const rule         = SpecHelper.getTreeRule(specFileName);
+
+describe(rule.name, () => {
+    beforeEach(() => {
+        SpecHelper.beforeEach();
+    });
+
+    afterEach(() => {
+        SpecHelper.afterEach();
+    });
+
+    it('detects hardcoded texts', () => {
+        const result = SpecHelper.parseAndApplyRuleToTemplate(rule, 0);
+
+        expect(result).toEqual([{
+            line        : 'I\'m a hardcoded-text',
+            lineNumber  : 4,
+            columnStart : -1,
+            length      : 34
+        }, {
+            line        : 'I\'m another hardcoded-text',
+            lineNumber  : 7,
+            columnStart : -1,
+            length      : 40
+        }]);
+    });
+});
