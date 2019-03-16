a97a94903a9330e906926bfec0563df67ecdc8bc
Fabiow Quixadá <ftquixada@gmail.com>
2019-03-16 11:27:44 -0300

    Rule Autofix - No Tabs

--- src/app/rules/line_by_line/no-tabs.js
+++ src/app/rules/line_by_line/no-tabs.js
@@ -1,13 +1,19 @@
 const SingleLineRulePrototype = require('../prototypes/SingleLineRulePrototype');
+const IndentRule              = require('../tree/indent');
 
 const ruleName    = require('path').basename(__filename).slice(0, -3);
 const description = 'Tab detected';
+const indent      = IndentRule.getIndentation();
 
 const Rule = Object.create(SingleLineRulePrototype);
 
 Rule.init(ruleName, description);
 
 Rule.isBroken = function(line) { return line.indexOf('\t') !== -1; };
+
+Rule.getFixedContent = function(fileContent) {
+    return fileContent.replaceAll('\t', indent);
+};
 
 Rule.getFirstOccurrence = function(line) {
 
