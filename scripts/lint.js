a6ec80b5820103a505e5fbd7adc2c693230da3a0
Fabiow Quixadá <ftquixada@gmail.com>
2019-05-04 11:55:41 -0300

    Internal Public API Call Fix

--- scripts/lint.js
+++ scripts/lint.js
@@ -2,8 +2,8 @@
 
 require('../src/app/util/NativeExtensionUtils');
 
-const Builder  = require('../publicApi').Builder;
-const exitCode = Builder.run();
+const IsmlLinter = require('../src/app/publicApi');
+const exitCode   = IsmlLinter.build();
 
 process.argv.forEach( val => {
     val === '--build' && process.exit(exitCode);
