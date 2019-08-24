cf9394c58259b713fc96a2924b7d30b7a6926368
Fabiow Quixadá <ftquixada@gmail.com>
2019-08-24 15:36:25 -0300

    Uncomment Production Spec

--- spec/app/ProductionSpec.js
+++ spec/app/ProductionSpec.js
@@ -1,94 +1,88 @@
-// const path         = require('path');
-// const fs           = require('fs-extra');
-// const rimraf       = require('rimraf');
-// const { execSync } = require('child_process');
-// const SpecHelper   = require('../SpecHelper');
-// const Constants    = require('../../src/app/Constants');
-// const ConfigUtils  = require('../../src/app/util/ConfigUtils');
+const path         = require('path');
+const fs           = require('fs-extra');
+const rimraf       = require('rimraf');
+const { execSync } = require('child_process');
+const SpecHelper   = require('../SpecHelper');
+const Constants    = require('../../src/app/Constants');
+const ConfigUtils  = require('../../src/app/util/ConfigUtils');
 
-// const targetDir             = path.join(Constants.clientAppDir, '..', Constants.sampleProductionProjectName);
-// const targetPackageBasePath = path.join(targetDir, 'package_base.json');
-// const targetPackagePath     = path.join(targetDir, 'package.json');
+const targetDir             = path.join(Constants.clientAppDir, '..', Constants.sampleProductionProjectName);
+const targetPackageBasePath = path.join(targetDir, 'package_base.json');
+const targetPackagePath     = path.join(targetDir, 'package.json');
 
-// describe('Production', () => {
-//     beforeEach(() => {
-//         deleteSampleProjectDir();
-//         const { version } = require(path.join(Constants.clientAppDir, 'package.json'));
-//         copySampleProject();
-//         setProductionVersion(version);
-//         installLinter();
-//         SpecHelper.beforeEach();
-//     });
+describe('Production', () => {
+    beforeEach(() => {
+        deleteSampleProjectDir();
+        const { version } = require(path.join(Constants.clientAppDir, 'package.json'));
+        copySampleProject();
+        setProductionVersion(version);
+        installLinter();
+        SpecHelper.beforeEach();
+    });
 
-//     afterEach(() => {
-//         removeConfigs();
-//         SpecHelper.afterEach();
-//         deleteSampleProjectDir();
-//     });
+    afterEach(() => {
+        removeConfigs();
+        SpecHelper.afterEach();
+        deleteSampleProjectDir();
+    });
 
-//     // TODO
-//     // it('raises an error if no config is set', () => {
-//     //     removeConfigs();
-//     //     const exitCode = runLinter();
-//     //     expect(exitCode).toEqual(1);
-//     // });
 
-//     it('runs successfully if a configuration is set', () => {
-//         initConfig();
+    it('runs successfully if a configuration is set', () => {
+        initConfig();
 
-//         const exitCode = runLinter();
+        const exitCode = runLinter();
 
-//         expect(exitCode).toEqual(0);
-//     });
-// });
+        expect(exitCode).toEqual(0);
+    });
+});
 
-// const copySampleProject = () => {
-//     const sourceDir = Constants.sampleProductionProjectDir;
+const copySampleProject = () => {
+    const sourceDir = Constants.sampleProductionProjectDir;
 
-//     fs.copySync(sourceDir, targetDir);
-// };
+    fs.copySync(sourceDir, targetDir);
+};
 
-// const setProductionVersion = version => {
+const setProductionVersion = version => {
 
-//     fs.copySync(targetPackageBasePath, targetPackagePath);
+    fs.copySync(targetPackageBasePath, targetPackagePath);
 
-//     const data   = fs.readFileSync(targetPackagePath, 'utf8');
-//     const result = data.replace(/{version}/g, version);
+    const data   = fs.readFileSync(targetPackagePath, 'utf8');
+    const result = data.replace(/{version}/g, version);
 
-//     fs.writeFileSync(targetPackagePath, result, 'utf8');
-// };
+    fs.writeFileSync(targetPackagePath, result, 'utf8');
+};
 
-// const installLinter = () => {
-//     return runCommand('npm run import');
-// };
+const installLinter = () => {
+    return runCommand('npm run import');
+};
 
-// const initConfig = () => {
-//     return runCommand('npm run init:isml');
-// };
+const initConfig = () => {
+    return runCommand('npm run init:isml');
+};
 
-// const runLinter = () => {
-//     return runCommand('npm run lint');
-// };
+const runLinter = () => {
+    return runCommand('npm run lint');
+};
 
-// const removeConfigs = () => {
-//     rimraf.sync(path.join(targetDir, Constants.configPreferredFileName));
-//     rimraf.sync(path.join(targetDir, Constants.configFileName));
-//     ConfigUtils.clearConfig();
-// };
+const removeConfigs = () => {
+    rimraf.sync(path.join(targetDir, Constants.configPreferredFileName));
+    rimraf.sync(path.join(targetDir, Constants.configFileName));
+    ConfigUtils.clearConfig();
+};
 
-// const deleteSampleProjectDir = () => {
-//     rimraf.sync(targetDir);
-// };
+const deleteSampleProjectDir = () => {
+    rimraf.sync(targetDir);
+};
 
-// const runCommand = inputCommand => {
-//     try {
-//         const prodRootDir = path.join('..', Constants.sampleProductionProjectName);
-//         const command     = `cd ${prodRootDir} && ${inputCommand}`;
+const runCommand = inputCommand => {
+    try {
+        const prodRootDir = path.join('..', Constants.sampleProductionProjectName);
+        const command     = `cd ${prodRootDir} && ${inputCommand}`;
 
-//         execSync(command);
+        execSync(command);
 
-//         return 0;
-//     } catch (err) {
-//         return err;
-//     }
-// };
+        return 0;
+    } catch (err) {
+        return err;
+    }
+};
